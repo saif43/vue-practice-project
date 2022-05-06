bbefd36fcc895b02365a561d400ae27f56f21e8b
Saif Ahmed Anik <saif.ahmed.anik.0@gmail.com>
2022-05-06 17:40:28 +0600

    10.3

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <ul>
+        <LearningResources v-for="item in storedResources" :key="item.id" :resource="item" />
+    </ul>
+</template>
+<script>
+import LearningResources from './components/LearningResources.vue'
+
+
+export default {
+    name: 'App',
+    components: {
+        LearningResources
+    },
+    data(){
+        return {
+            storedResources: [
+                {
+                    id: 1,
+                    title: 'Vue.js',
+                    description: 'Learn vue.js',
+                    link: 'https://vuejs.org',
+                },
+                {
+                    id: 2,
+                    title: 'Google',
+                    description: 'Learn to use google',
+                    link: 'https://google.com',
+                },
+            ]
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/components/LearningResources.vue
@@ -0,0 +1,25 @@
+<template>
+    <li>
+       <div>
+           <header>
+               <h3>{{resource.title}}</h3>
+               <button>Delete</button>
+           </header>
+       </div>
+       <p>{{resource.description}}</p>
+       <nav>
+           <a href="resource.link">View resource</a>
+       </nav>
+    </li>
+</template>
+<script>
+export default {
+    name: 'LearnResources',
+    props: {
+        resource: Object
+    }
+}
+</script>
+<style lang="">
+    
+</style>
